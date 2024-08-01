0f5cd93ef4d997f01468fa5236f4eecbc00ff07b
Bogdan Aleksandruk <aleksandruk04bogdan@gmail.com>
2024-08-01 07:28:38 +0300

    1.08
    colors + id + scatter up

--- app/app.js
+++ app/app.js
@@ -106,6 +106,7 @@ var download_text = document.getElementById('download-text')
 const downloadLink_server = document.getElementById('downloadLink-server');
 var WorkTask = [];
 var WorkResource = [];
+var all_id = [];
 // Обработчик нажатия на загрузку на сервер
 downloadLink_server.addEventListener("click", async function() {
   const { xml } = await bpmnModeler.saveXML();
@@ -126,12 +127,18 @@ downloadLink_server.addEventListener("click", async function() {
   }).done(async function(data) {
     var all_keys = [];
     var all_values = [];
-
+    
     WorkTask.push(data['WorkTask'])
     WorkResource.push(data['WorkResource'])
 
+    WorkTask[0].forEach((ind)=> {
+      all_id.push(ind['id']);
+    })
+
+
     console.log(WorkTask);
     console.log(WorkResource);
+    console.log(all_id);
     for (let key in data) {
       all_keys.push(key);
       all_values.push(data[key]);
@@ -148,12 +155,11 @@ downloadLink_server.addEventListener("click", async function() {
     }
     console.log(data_k);
     console.log(data_v);
+    await Scatterpolar(data_k, data_v);
+    await Gisto_hrzn(WorkTask, 'Gisto_task', 1);
+    await Gisto_hrzn(WorkResource, 'Gisto_resource', 1);
     save_content.style.display = 'none';
     window_graphic.style.display = "flex";
-    await Scatterpolar(data_k, data_v);
-    createTableBody(data_k, data_v);
-    Gisto_hrzn(WorkTask, 'Gisto_task', 1);
-    Gisto_hrzn(WorkResource, 'Gisto_resource', 1);
     console.log("Расчеты завершены");
 });
   }catch(error){
@@ -200,37 +206,18 @@ function Scatterpolar(data_k, data_v){
   }
   console.log(r_values_r);
     //r_values_r[i-1] = Array.from(r_values_r, x => x ?? 0);
-  let data = [
-    {
-      type: "scatterpolar",
-      name: "i=1",
-      r: r_values_r[0],
-      theta: th,
-      fill: "toself",
-    },
-    {
-      type: "scatterpolar",
-      name: "i=2",
-      r: r_values_r[1],
-      theta: th,
-      fill: "toself",
-    },
-    {
-      type: "scatterpolar",
-      name: "i=3",
-      r: r_values_r[2],
-      theta: th,
-      fill: "toself",
-    },
-    {
-      type: "scatterpolar",
-      name: "i=4",
-      r: r_values_r[3],
-      theta: th,
-      fill: "toself"
+  let data = [];
+  let data_sets = {r:[], theta:[], type: '', 
+      fill: '',
+      name:''}
+  for (let i = 0; i < all_id.length; i++) {
+      data_sets = {r:r_values_r[i], theta:th, type: 'scatterpolar', 
+        fill: 'toself',
+        name: 'i = ' + String(i+1)}
+      data[i] = data_sets;
     }
-  ]
   Plotly.newPlot('Scatterplot', data);
+  createTableBody(th, data_values);
 }
 
 function createTableBody(data_keys, data_values) {
@@ -253,11 +240,16 @@ var n = 0;
 function Gisto_hrzn(Work_datas, name, grapf_id){
   let work_data = Work_datas[0][grapf_id-1];
   let datas = [];
+  let color_toappend = [];
   for(let key in work_data){
     if (key === 'data') {
       datas.push(work_data[key]);
     }
+    else if(key === 'colors'){
+      color_toappend.push(work_data[key]);
+    }
   }
+  console.log(color_toappend);
   console.log(work_data);
   console.log(datas);
   let y_toappend =[];
@@ -273,6 +265,7 @@ function Gisto_hrzn(Work_datas, name, grapf_id){
     }
     x_toappend.push(ind['Start']);
     x_toappend.push(ind['Finish']);
+    
   })
   
   console.log(y_toappend);
@@ -289,9 +282,11 @@ function Gisto_hrzn(Work_datas, name, grapf_id){
       }
     }
   }*/
- let data= [];
+ let data = [];
  let temp_x = [];
  let temp_y = [];
+ let temp_color = [];
+ let temp_name = [];
  let j = 0;
  let data_sets = {x:[], y:[], type: '', 
   opacity: 0.5,
@@ -302,13 +297,16 @@ function Gisto_hrzn(Work_datas, name, grapf_id){
   temp_x.push(x_toappend[j+1]);
   temp_y.push(y_toappend[j]);
   temp_y.push(y_toappend[j+1]);
+  temp_color.push(color_toappend[i]);
   j += 2;
   data_sets = {x: temp_x, y: temp_y, type: 'scatter', 
-    opacity: 0.5, line: {color: 'red', width: 20 }, 
-    mode: 'lines', name:j}
+    opacity: 0.5, line: {color: temp_color, width: 20 }, 
+    mode: 'lines', name: temp_y[i]}
   data[i] = data_sets;
   temp_x = [];
   temp_y = [];
+  temp_color = [];
+  temp_name = [];
  }
  let title = '';
   if (n%2 == 0) {
